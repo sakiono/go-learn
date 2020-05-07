6ef7a722130f7c60324fe47658db4feb7b8d8942
saki.ono <saki.ono@sakiononoMacBook-Air.local>
2020-05-07 15:58:45 +0900

    おみくじ機能の追加

--- try_http_template.go
+++ try_http_template.go
@@ -11,25 +11,57 @@ html/templateを利用しよう
 package main
 
 import (
+	"math/rand"
 	"net/http"
 	"html/template"
+	"time"
 )
+
+type Result struct {
+	Name    string
+	Omikuji string
+}
 
 //クリエストが来たらおみくじ結果を返す
 func handler4(w http.ResponseWriter, r *http.Request) {
 
-	name := r.FormValue("p")
-
-	if name == ""{
-		name = "ゲスト"
+	result := Result{
+		Name:    r.FormValue("p"), //パラメータの値(名前)
+		Omikuji: omikuji4(), //おみくじ結果
 	}
 
-	tmpl := template.Must(template.New("omikuji").Parse("<html><body>{{.}}さんの運勢は「<b>大吉</b>」です </body></html>"))
-	tmpl.Execute(w, name)
+
+	if result.Name == ""{
+		result.Name = "ゲスト"
+	}
+
+	tmpl := template.Must(template.New("omikuji").Parse("<html><body>{{.Name}}さんの運勢は「<b>{{.Omikuji}}</b>」です </body></html>"))
+	tmpl.Execute(w, result)
+
+}
+
+func omikuji4()string {
+	randomNum := rand.Intn(5)
+
+	switch randomNum {
+	case 0:
+		return "凶"
+	case 1:
+		return "吉"
+	case 2:
+		return "小吉"
+	case 3:
+		return "中吉"
+	default:
+		return "大吉"
+	}
 
 }
 
 func main() {
+	//乱数の種
+	rand.Seed(time.Now().UnixNano())
+
 	//ルーティング
 	http.HandleFunc("/", handler4)
 

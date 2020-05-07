1546e45f2f14f83d73a5eab7c4b19530b57f905c
saki.ono <saki.ono@sakiononoMacBook-Air.local>
2020-05-07 15:59:12 +0900

    おみくじ機能の追加

--- try_http_omikuji2.go
+++ try_http_omikuji2.go
@@ -10,23 +10,45 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 //クリエストが来たらおみくじ結果を返す
 func handler3(w http.ResponseWriter, r *http.Request) {
 
 	name := r.FormValue("p")
-	
+
 	if name == ""{
 		name = "ゲスト"
 	}
 
-	fmt.Fprintf(w,"%sさんの運勢は「大吉」です！\n", name)
+	fmt.Fprintf(w,"%sさんの運勢は「%s」です！\n", name,omikuji3())
 
 }
 
+func omikuji3() string {
+	randomNum := rand.Intn(5)
+
+	switch randomNum {
+	case 0:
+		return "凶"
+	case 1:
+		return "吉"
+	case 2:
+		return "小吉"
+	case 3:
+		return "中吉"
+	default:
+		return "大吉"
+	}
+}
+
 func main() {
+	//乱数の種
+	rand.Seed(time.Now().UnixNano())
+
 	//ルーティング
 	http.HandleFunc("/", handler3)
 
